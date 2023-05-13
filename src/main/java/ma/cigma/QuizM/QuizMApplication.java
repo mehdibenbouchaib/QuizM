e58bac0a4a7ac2e5d9a7380b3664fd01169a42f3
mehdi <mehdi.benbouchaib@gmail.com>
2023-05-13 12:57:53 +0100

    first commit

---
+++ src/main/java/ma/cigma/QuizM/QuizMApplication.java
@@ -0,0 +1,13 @@
+package ma.cigma.QuizM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/cigma/QuizM/QuizMApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.cigma.QuizM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
